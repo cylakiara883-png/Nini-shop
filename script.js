fb2b193de4a6798a7e1231c06e71a3cdf8b5b445
cylakiara883-png <cylakiara883@gmail.com>
2026-08-13 11:54:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let panier = [];
 
 const boutons = document.querySelectorAll(".product button");
+const panierItems = document.getElementById("panier-items");
+const totalElement = document.getElementById("total");
+const viderPanier = document.getElementById("vider-panier");
+
+
+// AJOUTER UN PRODUIT AU PANIER
 
 boutons.forEach((bouton) => {
+
     bouton.addEventListener("click", () => {
 
         const produit = bouton.parentElement;
 
         const nom = produit.querySelector("h3").textContent;
+
         const prix = parseFloat(
             produit.querySelector(".price").textContent
                 .replace("$", "")
@@ -18,22 +26,72 @@ boutons.forEach((bouton) => {
             prix: prix
         });
 
-        alert(nom + " a été ajouté au panier 🛒");
-
         afficherPanier();
+
+        alert(nom + " a été ajouté au panier 🛒");
     });
+
 });
 
 
+// AFFICHER LE PANIER
+
 function afficherPanier() {
 
-    console.log("Panier :", panier);
+    panierItems.innerHTML = "";
+
+    if (panier.length === 0) {
+
+        panierItems.innerHTML =
+            "<p>Votre panier est vide.</p>";
+
+        totalElement.textContent = "0 $";
+
+        return;
+    }
 
     let total = 0;
 
-    panier.forEach((article) => {
+    panier.forEach((article, index) => {
+
         total += article.prix;
+
+        const div = document.createElement("div");
+
+        div.classList.add("panier-article");
+
+        div.innerHTML = `
+            <span>${article.nom}</span>
+            <span>${article.prix} $</span>
+
+            <button onclick="supprimerArticle(${index})">
+                ❌
+            </button>
+        `;
+
+        panierItems.appendChild(div);
     });
 
-    console.log("Total :", total + " $");
+    totalElement.textContent = total + " $";
 }
+
+
+// SUPPRIMER UN ARTICLE
+
+function supprimerArticle(index) {
+
+    panier.splice(index, 1);
+
+    afficherPanier();
+}
+
+
+// VIDER LE PANIER
+
+viderPanier.addEventListener("click", () => {
+
+    panier = [];
+
+    afficherPanier();
+
+});
