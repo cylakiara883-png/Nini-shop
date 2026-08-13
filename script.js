d33bfcda28eeabd7d2edf9af702f8e2754599f7b
cylakiara883-png <cylakiara883@gmail.com>
2026-08-13 11:50:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-#
+let panier = [];
+
+const boutons = document.querySelectorAll(".product button");
+
+boutons.forEach((bouton) => {
+    bouton.addEventListener("click", () => {
+
+        const produit = bouton.parentElement;
+
+        const nom = produit.querySelector("h3").textContent;
+        const prix = parseFloat(
+            produit.querySelector(".price").textContent
+                .replace("$", "")
+        );
+
+        panier.push({
+            nom: nom,
+            prix: prix
+        });
+
+        alert(nom + " a été ajouté au panier 🛒");
+
+        afficherPanier();
+    });
+});
+
+
+function afficherPanier() {
+
+    console.log("Panier :", panier);
+
+    let total = 0;
+
+    panier.forEach((article) => {
+        total += article.prix;
+    });
+
+    console.log("Total :", total + " $");
+}
